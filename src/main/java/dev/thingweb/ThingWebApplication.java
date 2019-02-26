7a37b0cbc557785e10659f29753f5e193db0da20
parksangwon <ourtree4@gmail.com>
2019-02-26 23:25:31 +0900

    Initial commit

---
+++ src/main/java/dev/thingweb/ThingWebApplication.java
@@ -0,0 +1,13 @@
+package dev.thingweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThingWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThingWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/thingweb/ThingWebApplicationTests.java
@@ -0,0 +1,16 @@
+package dev.thingweb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ThingWebApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
